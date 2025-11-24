3a2f0863678c7e83f1f334835957545ea5bc2e68
dialite <raymondolisa01@gmail.com>
2025-11-24 08:16:21 +0100

    Provide workspace router with user access protection middleware

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,14 @@
+export const protectUserAccess = async (req, res, next) => {
+  try {
+    const { userId } = await req.auth();
+
+    if (!userId) {
+      return res.status(401).json({ message: "unauthorized" });
+    }
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ message: error.code || error.message });
+  }
+};
